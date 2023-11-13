fb629eb9e75111dfdb49ca596061ba8bd0a8c801
Aman Gupta <Ashish.Kumar4.Contractor@pepsico.com>
2023-11-13 23:13:11 +0530

    first commit

---
+++ src/main/java/com/devopsinsiders/DevOps_Insiders_Java_Demo/DevOpsInsidersJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devopsinsiders.DevOps_Insiders_Java_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsInsidersJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsInsidersJavaDemoApplication.class, args);
+	}
+
+}
